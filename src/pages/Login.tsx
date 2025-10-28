615a5494229c067943a92fccc06b846fe8b20ff7
mersalhussin <mersalhussin@gmail.com>
2025-10-29 02:42:16 +0300

    Validation With React Hook Form && Form Input Errors

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Button from "../components/ui/Button";
 import Input from "../components/ui/Input";
 
@@ -13,6 +14,12 @@ const LoginPage=()=>{
           <Input placeholder="Emaill address" />
           <Input placeholder="Password" />
           <Button fullWidth>Login</Button>
+               <p className="text-center text-sm text-gray-500 space-x-2">
+          <span>No account?</span>
+          <Link to={"/register"} className="underline text-indigo-600 font-semibold">
+            Register
+          </Link>
+        </p>
       </form>
     </div>
   );
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,22 @@
+import { useForm, SubmitHandler } from "react-hook-form"
 import Button from "../components/ui/Button";
 import Input from "../components/ui/Input";
+import { Link } from "react-router-dom";
 
-  // Renders
+
+interface IFormInput {
+  username: string;
+  email: string;
+  password: string;
+}
+
+
+// Renders
 const  RegisterPage=()=>{
-  
+  const { register, handleSubmit , formState:{errors} } = useForm<IFormInput>()
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+
+  console.log(errors);
 
   return (
     <div className="max-w-md mx-auto">
@@ -12,13 +25,19 @@ const  RegisterPage=()=>{
         Register to get access!
       </h2>
       
-      <form className="space-y-4">
-        <Input placeholder="Username" />
-        <Input placeholder="Emaill address" />
-        <Input placeholder="Password" />
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        <Input placeholder="Username" {...register("username" , {required: "Username is required"})}/>
+        <p>{errors?.username?.message}</p>
+        <Input placeholder="Emaill address" {...register("email" , {required: "Email is required"})} />
+        <Input placeholder="Password" {...register("password" , {required: "Password is required"})} />
 
-       <Button fullWidth>REister</Button>
-
+       <Button fullWidth>Register</Button>
+                  <p className="text-center text-sm text-gray-500 space-x-2">
+          <span>have an account?</span>
+          <Link to={"/login"} className="underline text-indigo-600 font-semibold">
+            Login
+          </Link>
+        </p>
       </form>
     </div>
   );
