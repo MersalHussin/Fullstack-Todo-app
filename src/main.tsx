484e6c012a1e23a3bf5a02986ae93e04e3a0a83f
mersalhussin <mersalhussin@gmail.com>
2025-11-27 03:31:44 +0200

    intro React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
