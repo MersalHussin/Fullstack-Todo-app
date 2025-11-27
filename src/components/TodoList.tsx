6e74cd25111c6316fd95295007c0c2cd723f5a6b
mersalhussin <mersalhussin@gmail.com>
2025-11-27 03:17:19 +0200

    Fetch Data Using Use Effect

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,6 +5,7 @@ import axiosinstance from "../config/axios config"
 
 const TodoList = () => {
 const [todos, setTodos] = useState([]);
+const [isLoading, setIsLoading] = useState(true); 
 const storageKey = "loggedinUser";
 const userDataString =  localStorage.getItem(storageKey);
 const userData = userDataString ? JSON.parse(userDataString) : null;
@@ -17,23 +18,28 @@ useEffect(() =>{
       }
     })
     .then((res => setTodos(res.data.todoayas)))
-    .catch(err => console.log("This is error", err));
+    .catch(err => console.log("This is error", err))
+    .finally(() => setIsLoading(false));
   }catch(error){
     console.log(error);
   }
 },[userData?.jwt])
 
+if(isLoading){
+  return <p>Loading...</p>
+}
+
   return (
     <>
     <div className="flex flex-col w-full flex-wrap items-center justify-between">
-      {todos.map(todo => (
+      {todos.length ? todos.map(todo => (
 
       <div key={todo.id} className="flex items-center justify-between w-full mb-4 p-4 border rounded-md">
         <p className="w-full font-semibold"> 1- {todo.title}</p>
         <Button size={"sm"}>Edit</Button>
         <Button variant={"danger"} size={"sm"}>Remove</Button>
     </div>
-      ))}
+      )) : (<p>No todos found.</p>)}
       </div>
  
        
