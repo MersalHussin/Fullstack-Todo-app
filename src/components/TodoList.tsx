d1d4ed9ff1758a3ce05295e59432c9ab38c423ee
mersalhussin <mersalhussin@gmail.com>
2025-11-27 04:20:52 +0200

    Error Handling And Requst Options

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,7 +6,7 @@ const TodoList = () => {
 const storageKey = "loggedinUser";
 const userDataString =  localStorage.getItem(storageKey);
 const userData = userDataString ? JSON.parse(userDataString) : null;
-const {isLoading, data,error} = useQuery({
+const {isLoading, data} = useQuery({
   queryKey: ['todo'],
   queryFn:async() =>{
     const {data}= await axiosinstance.get("/users/me?populate=todoayas",{
@@ -19,22 +19,19 @@ const {isLoading, data,error} = useQuery({
   }
 })
 
-console.log(isLoading,data,error);
-
 if(isLoading) return <p>Loading....</p>
-if(error) return "An error has occurred" + error.message
 return (
     <>
     <div className="flex flex-col w-full flex-wrap items-center justify-between">
      
-     {data.todoayas.map(todo =>(
+     {data.todoayas.length ? data.todoayas.map(todo =>(
 
        <div key={todo.id}  className="flex items-center justify-between w-full mb-4 p-4 border rounded-md">
         <p className="w-full font-semibold"> 1- {todo.title}</p>
         <Button size={"sm"}>Edit</Button>
         <Button variant={"danger"} size={"sm"}>Remove</Button>
     </div>
-      ))}
+      )): <p>No Todos yet</p>}
       </div>
  
        
